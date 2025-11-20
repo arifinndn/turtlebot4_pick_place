efa9dcb6df5c8b5f932fc0419b0ce880bf6481fa
arifinndn <148372931+arifinndn@users.noreply.github.com>
2025-11-20 22:02:25 +0700

    Update localization.launch.py

--- launch/localization.launch.py
+++ launch/localization.launch.py
@@ -18,7 +18,7 @@ ARGUMENTS = [
 
 
 def generate_launch_description():
-    pkg_turtlebot4_navigation = get_package_share_directory('turtlebot4_uts')
+    pkg_turtlebot4_navigation = get_package_share_directory('turtlebot4_nav_uts')
     pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
 
     localization_params_arg = DeclareLaunchArgument(
